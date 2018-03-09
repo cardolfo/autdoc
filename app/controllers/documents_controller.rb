44a32f5545d8fe8e8e15c8cbf05e05882849ce9f
Adolfo <cardolfo@gmail.com>
2018-03-09 09:27:34 -0300

    Transformando a aplicação em API

--- app/controllers/documents_controller.rb
+++
@@ -1,9 +0,0 @@
-class DocumentsController < ApplicationController
-  def create
-    if params[:document][:file] 
-      file_name = params[:document][:file_name]
-      file = File.new("/home/cardolfo/temp/#{file_name}","rw")
-      file.write(Base64.decode64(params[:document][:file]))
-    end
-  end
-end
--- test/controllers/documents_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DocumentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get documents_create_url
-    assert_response :success
-  end
-
-end
