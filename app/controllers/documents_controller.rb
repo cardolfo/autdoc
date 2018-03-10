47d2b771512ee2c9500cd5647fc56385ed446f05
Carlos Adolfo <cardolfo@gmail.com>
2018-03-09 21:22:32 -0300

    Adicionando verificação de parametros e melhorando o código

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,12 +1,29 @@
 class DocumentsController < ApplicationController
+  
   def create
-    if params[:document][:file] 
-      file_name = params[:document][:file_name]
-      content_file_str = Base64.decode64(params[:document][:file])           
-      File.open("/home/tj.ce.gov.br/8880/Temp/#{file_name}","w:UTF-8") do |file|
-        file.write(content_file_str.force_encoding("UTF-8"))        
-        file.close
-      end      
+    
+    file_content_encoded_base64 = document_params[:file]
+
+    if file_content_encoded_base64 
+      #file_name = document_params[:file_name]   
+      save(file_content_decoded_base64(file_content_encoded_base64))
     end
   end
+
+  def file_content_decoded_base64(file_content_encoded_base64)
+    Base64.decode64(file_content_encoded_base64) 
+  end
+
+  def save(file_content)
+    #File.open("/home/tj.ce.gov.br/8880/Temp/#{file_name}","w:UTF-8") do |file|
+    File.open("/home/cardolfo/Temp/Novo.pdf","w:UTF-8") do |file|
+      file.write(file_content.force_encoding("UTF-8"))        
+      file.close
+    end
+  end
+
+  def document_params
+    params.require(:document).permit(:file, :file_name)
+  end
+
 end
